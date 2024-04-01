ef750d46da72f643ec923855c8ca592ecdd2d20d
Asya150821 <tgmenshikova@mail.ru>
2024-04-01 11:57:50 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        public class Car {
+            private String bibika;
+            private int speed;
+
+            public Car(String bibika, int speed) {
+                this.bibika = bibika;
+                this.speed = speed;
+            }
+
+            public Car(int speed) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                this.speed = speed;
+            }
+
+            public Car() {
+            }
+
+            public Car(String bibika) {
+
+                this.bibika = bibika;
+            }
+
+            public void accelerate(int kolvo) {
+
+            }
+        }
+    }
+}
